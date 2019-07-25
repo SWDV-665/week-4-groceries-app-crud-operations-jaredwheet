1e937c08450a3f16147a7e4164349b82e7675415
Jared Wheet <jaredwheet@gmail.com>
2019-07-24 19:52:23 -0700

    Week4 Work

---
+++ Week4/src/app/tab3/tab3.page.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { GroceriesServiceService} from '../groceries-service.service'
+import {InputDialogServiceService} from '../input-dialog-service.service'
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+
+export class Tab3Page {
+  title = "Grocery List";
+
+  loadItems(){
+    return this.dataService.getItems()
+  }
+
+  async removeItem(item, index) {
+    console.log("Removing Item - ", item, index)
+    
+    const toast = await this.toastController.create({
+      message: 'Removed Grocery Item - ' + item.name,
+      duration: 2000
+    });
+    toast.present();
+    this.dataService.removeItem(index)
+  }
+
+  async editItem(item, index) {
+    console.log("Editing - ", item, index)
+    
+    const toast = await this.toastController.create({
+      message: 'Editing Grocery Item - ' + item.name,
+      duration: 2000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item,index)
+    
+  }
+
+  async addItem(){
+    console.log("Adding Item")
+    this.inputDialogService.showPrompt()
+  }
+
+
+
+  constructor(public toastController: ToastController, public alertController: AlertController, public dataService: GroceriesServiceService, public inputDialogService: InputDialogServiceService) { }
+
+}
